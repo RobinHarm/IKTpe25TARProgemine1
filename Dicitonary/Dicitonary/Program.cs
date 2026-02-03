a6412cc1268e691579723cd3848dcc710e2d029e
Kundratsk <robinpoiss99@gmail.com>
2026-02-03 15:13:02 +0200

    Dictionary
    mingi Dictionary

---
+++ Dicitonary/Dicitonary/Program.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Dicitonary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dictionary ja foreach");
+            //var on muutuja
+            var domains = new Dictionary<string, string>()
+            {
+              {"fi", "Finland" },
+              {"se", "Sweden" },
+              {"de", "German" },
+              {"fr", "France" },
+              {"es", "Spain" }
+            };
+            int i = 1;
+            //kasutate foreachi ja juurde tuleb lisada kolmas rida
+            foreach (var domain in domains)
+            {
+                    
+                Console.WriteLine($"{domain.Key} - {domain.Value} - {i}");       
+                i++;
+            }
+        }
+    }
+}
