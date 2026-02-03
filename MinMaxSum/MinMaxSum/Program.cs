6f93478aa18bd72839eaf37ae2c35b8bf83fdebc
Kundratsk <robinpoiss99@gmail.com>
2026-02-03 15:40:56 +0200

    numbri sorteerija
    sorteerib numbreid

---
+++ MinMaxSum/MinMaxSum/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace MinMaxSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("List numbrites");
+
+            int[] numbers = new int[10] { 2, 10, 15, 5, 4, 3, 1, 9, 8, 12 };
+            ////max
+            //Console.WriteLine(numbers.Max());
+            ////min
+            //Console.WriteLine(numbers.Min());
+            ////sum
+            //Console.WriteLine(numbers.Sum());
+            ////average
+            //Console.WriteLine(numbers.Average());
+
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Sorteerib numbrid alates väiksemast suuremani");
+
+            //peate kasutama Array ja Sort
+            Array.Sort(numbers);
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+             
+            //sorteerib numbrid alates suuremast väiksemani
+
+        }
+    }
+}
